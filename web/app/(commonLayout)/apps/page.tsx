b3a98a2c75879f1a96516f72e2eef72cf074be1d
Damon L <vvs3912@outlook.com>
2025-03-11 10:39:41 +0800

    兼容新版修改

--- web/app/(commonLayout)/apps/page.tsx
+++ web/app/(commonLayout)/apps/page.tsx
@@ -16,10 +16,13 @@ const AppList = () => {
     <div className='relative flex flex-col overflow-y-auto bg-background-body shrink-0 h-0 grow'>
       <Apps />
       {systemFeatures.license.status === LicenseStatus.NONE && <footer className='px-12 py-6 grow-0 shrink-0'>
-        <h3 className='text-xl font-semibold leading-tight text-gradient'>{t('app.join')}</h3>
-        <p className='mt-1 system-sm-regular text-text-tertiary'>{t('app.communityIntro')}</p>
+        {/*<h3 className='text-xl font-semibold leading-tight text-gradient'>{t('app.join')}</h3>*/}
+        {/*<p className='mt-1 system-sm-regular text-text-tertiary'>{t('app.communityIntro')}</p>*/}
+        <h3 className='text-xl font-semibold leading-tight text-gradient'>MBM跨国财阀集团</h3>
+        <p className='mt-1 text-sm font-normal leading-tight text-gray-700'>汇集全球顶尖人才，我们立志构筑一台前所未有的人工智能巨擘，其创新的力量将重新定义未来，震撼世界，引领人类进入一个崭新的智能时代。</p>
+
         <div className='flex items-center gap-2 mt-3'>
-          <Link className={style.socialMediaLink} target='_blank' rel='noopener noreferrer' href='https://github.com/langgenius/dify'>
+          <Link className={style.socialMediaLink} target='_blank' rel='noopener noreferrer' href='https://openai.mbmzone.com'>
             <RiGithubFill className='w-5 h-5 text-text-tertiary' />
           </Link>
           <Link className={style.socialMediaLink} target='_blank' rel='noopener noreferrer' href='https://discord.gg/FngNHpbcY7'>
